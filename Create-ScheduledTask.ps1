a4d421fc7f5c33b9a95c34ee8f1e3fc187d89e1b
Matt Gaillardetz <matthew.gaillardetz@tylertech.com>
2022-11-18 11:36:19 -0500

    Replaced hard coded variables

--- Create-ScheduledTask.ps1
+++ Create-ScheduledTask.ps1
@@ -1,13 +1,13 @@
 # create task with the account
-$ServerList = "FDVMMUNAPP5"
+$ServerList = "server","name","for","each","comma","seperated","host"
 $session = New-PsSession -ComputerName $ServerList
 
 $CreateScheduledTask = ({
-    $principal = New-ScheduledTaskPrincipal -UserId 'TYLER\tdo.ecqservice$' -LogonType Password
+    $principal = New-ScheduledTaskPrincipal -UserId 'DOMAIN\gmsa$' -LogonType Password
     $TaskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8 -ExecutionTimeLimit 06:00:00 -AllowStartIfOnBatteries
     $trigger = New-ScheduledTaskTrigger -At 06:00:01 -Daily
-    $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-File "D:\cmd\Get-HomeFolderSizes.ps1"'
-    $Task = Register-ScheduledTask "GetHomeFolderSizes2" -Action $action -Trigger $trigger -Principal $principal -Settings $TaskSettings
+    $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-File "D:\path\to\.ps1"'
+    $Task = Register-ScheduledTask "TaskName" -Action $action -Trigger $trigger -Principal $principal -Settings $TaskSettings
 })
 
 Invoke-Command -session $session -scriptblock $CreateScheduledTask
